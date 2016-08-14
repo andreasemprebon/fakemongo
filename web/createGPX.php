e3823935f0d61d7fe42551e2e1ea81af76ec6ca3
Andrea Semprebon <andreasemprebon@gmail.com>
2016-08-14 11:03:38 +0200

    Added autopilot (beta)

--- web/createGPX.php
+++ web/createGPX.php
@@ -1,13 +1,15 @@
 <?php
 	
 	$template = "<gpx><wpt lat=\"{{__LAT__}}\" lon=\"{{__LON__}}\"></wpt></gpx>";
-
-	$content = str_replace("{{__LAT__}}", $_GET['lat'], $template);
-	$content = str_replace("{{__LON__}}", $_GET['lon'], $content);
+	
+	$content = str_replace("{{__LAT__}}", $_POST['lat'], $template);
+	$content = str_replace("{{__LON__}}", $_POST['lon'], $content);
 
 	$fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/playerLocation.gpx", "wb");
 	fwrite($fp, $content);
 	fclose($fp);
-
-	exec('osascript /Users/Andrea/Documents/Sviluppo/iOS/FakemonGo/web/updateLocation.applescript');
+	
+	exec('osascript -s o /Users/Andrea/Documents/Sviluppo/iOS/FakemonGo/web/updateLocation.applescript 2>&1', $output, $return_var);
+	
+	echo $return_var;
 ?>
